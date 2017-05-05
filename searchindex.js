b6ddbc87ceb064003f9d02b42a2dfb4bad302861
qrvaelet <github-69hd767i@liszy.hu>
2017-05-05 13:41:07 +0200

    first gen

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to the unofficial NixOS documentation!"],titleterms:{document:0,indic:0,nixo:0,tabl:0,unoffici:0,welcom:0}})
